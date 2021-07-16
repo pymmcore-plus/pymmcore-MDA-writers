f9f0cb3483dfea5e685d06acea7d6157d81822c1
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2021-07-16 12:40:57 -0400

    use version in install

--- pymmcore_mda_writers/_version.py
+++ pymmcore_mda_writers/_version.py
@@ -1,1 +1,2 @@
-__version__= "0.1.1"
+version_info = (0, 1, 1)
+__version__ = ".".join(map(str, version_info))
--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
-import setuptools
+from os import path
 
-setuptools.setup()
+from setuptools import setup
+
+# extract version
+VERSION_PATH = path.realpath("pymmcore_mda_writers/_version.py")
+version_ns = {}
+with open(VERSION_PATH, encoding="utf8") as STREAM:
+    exec(STREAM.read(), {}, version_ns)
+version = version_ns["__version__"]
+
+if __name__ == "__main__":
+    setup(
+        version=version_ns["__version__"],
+    )
