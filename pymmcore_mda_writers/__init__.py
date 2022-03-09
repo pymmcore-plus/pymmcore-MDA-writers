f77875d69396901466bd0e3b442095e3b9dfe12e
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2022-03-08 23:57:45 -0500

    more tests

--- pymmcore_mda_writers/__init__.py
+++ pymmcore_mda_writers/__init__.py
@@ -1,7 +1,7 @@
 try:
     from ._version import version as __version__
 except ImportError:
-    __version__ = "unknown"
+    __version__ = "unknown"  # pragma: no cover
 __author__ = "Ian Hunt-Isaak"
 __email__ = "ianhuntisaak@gmail.com"
 
